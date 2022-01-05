d7645eaa2491f431212dadc0d7b8748b7bb53012
mzomeno- <1veleita1@gmail.com>
2022-01-05 14:31:22 +0100

    Added vector attributes test

--- tests/test_vector.cpp
+++ tests/test_vector.cpp
@@ -1,6 +1,16 @@
 #include "test_vector.hpp"
 
+template <class T>
+void print_attributes(NAMESPACE::vector<T> &vct)
+{
+    std::cout << "Empty vector: " << vct.empty() << std::endl;
+    std::cout << "Vector size: " << vct.size() << std::endl;
+    std::cout << "Vector max size: " << vct.max_size() << std::endl;
+    std::cout << "Vector capacity: " << vct.capacity() << std::endl;
+}
+
 void test_v_constructors()
 {
     NAMESPACE::vector<int> default_vector;
+    print_attributes(default_vector);
 }
