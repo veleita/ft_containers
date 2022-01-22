a8dc093cb0cbd9d334953ed854ca138619627758
mzomeno- <mzomeno-@student.42madrid.com>
2022-01-22 06:57:04 +0100

    Tests are now more simple and show usecs

--- tests/main.cpp
+++ tests/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mzomeno- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 19:05:11 by mzomeno-          #+#    #+#             */
-/*   Updated: 2022/01/22 06:48:50 by mzomeno-         ###   ########.fr       */
+/*   Updated: 2022/01/22 06:55:51 by mzomeno-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,6 @@ typedef void (*test)(void);
 
 int main()
 {
-	#if ORIGINAL == 1
-		std::cout << "Executing STL version...\n";
-	#else
-		std::cout << "Executing FT version...\n";
-	#endif
 
 	struct timeval	tv;
 	unsigned long	before;
@@ -32,49 +27,36 @@ int main()
 
 	std::cout << std::boolalpha;
 
-	std::cout << "UTILS TEST\n";
-	std::cout << "===========================\n\n";
-	test utils[1] = {
-				&test_is_integral,
-			//	&test_enable_if,
-			//	&test_pair,
-			//	&test_make_pair,
-			//	&test_equal,
-			//	&test_lexicographical_compare
-			};
-
 	gettimeofday(&tv, NULL);
 	before = tv.tv_sec * 1000000 + tv.tv_usec;
+
+	std::cout << "UTILS TEST\n";
+	std::cout << "===========================\n\n";
+	test utils[6] = {
+				&test_is_integral,
+				&test_enable_if,
+				&test_pair,
+				&test_make_pair,
+				&test_equal,
+				&test_lexicographical_compare
+			};
 
 	for (int i = 0; i < 1; i++)
 	{
 		(*utils[i])();
 		std::cout << "---------------------------\n\n";
 	}
-	gettimeofday(&tv, NULL);
-	after = tv.tv_sec * 1000000 + tv.tv_usec;
-	execution_time = before - after;
-    std::cout << " Execution time for utils is "
-				<< execution_time/1000000 << " seconds\n";
 
 
 	std::cout << "\nITERATOR TEST\n";
 	std::cout << "===========================\n";
 	test iterator[1] = {&test_iter_traits};
 
-	gettimeofday(&tv, NULL);
-	before = tv.tv_sec * 1000000 + tv.tv_usec;
-
 	for (int i = 0; i < 1; i++)
 	{
 		(*iterator[i])();
 		std::cout << "---------------------------\n\n";
 	}
-	gettimeofday(&tv, NULL);
-	after = tv.tv_sec * 1000000 + tv.tv_usec;
-	execution_time = before - after;
-    std::cout << " Execution time for iterators is "
-				<< execution_time/1000000 << " seconds\n";
 
 
 	std::cout << "\nVECTOR TEST\n";
@@ -87,19 +69,23 @@ int main()
 						&test_v_modifiers
 					};
 
-	gettimeofday(&tv, NULL);
-	before = tv.tv_sec * 1000000 + tv.tv_usec;
-
 	for (int i = 0; i < 5; i++)
 	{
 		(*vector[i])();
 		std::cout << "---------------------------\n\n";
 	}
+
+
 	gettimeofday(&tv, NULL);
 	after = tv.tv_sec * 1000000 + tv.tv_usec;
 	execution_time = before - after;
-    std::cout << " Execution time for vector is "
-				<< execution_time/1000000 << " seconds\n";
+	#if ORIGINAL == 1
+		std::cout << "Execution time for STL version is ";
+	#else
+		std::cout << "Execution time for FT version is ";
+	#endif
+	std::cout << execution_time/1000000 << " secs and "
+				<< execution_time%1000000 << " usecs\n";
 
 	return 0;
 }
